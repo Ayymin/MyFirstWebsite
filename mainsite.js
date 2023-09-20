5b463798eec217d0fe0457e560d5b409f4f5d6d7
Ayyminq <amin.dahir@hotmail.com>
2023-09-20 22:37:30 +0200

    Added button to html, no functionality yet

--- mainsite.js
+++ mainsite.js
@@ -1,14 +1,20 @@
 const portraitImg = document.querySelector('.portraits-container img');
-const changePortraitBtn = document.getElementById("changePortraitBtn")
 const portraitPaths = ["Portraits/cutevieradoodle.png", "/Portraits/icanexplain.png", "/Portraits/miqofaces.png"];
-
+const changePortraitBtn = document.getElementById("changePortraitBtn");
 let currentPortraitIndex = 0;
+
+
+const music = ["Music/Persona 5 - Beneath The Mask (Rain).mp3", "Music/Gil Scott-Heron & Brian Jackson - Corners (320 kbps).mp3"];
+const pauseBtn = document.getElementById("pauseButton");
+let currentSongIndex = 0;
+
 
 changePortraitBtn.addEventListener('click', () => {
     currentPortraitIndex++;
     if(currentPortraitIndex >= portraitPaths.length) {
         currentPortraitIndex =0;
-        console.log("yooo")
+        console.log("yooo");
     }
     portraitImg.src = portraitPaths[currentPortraitIndex];
 });
+
